29a21cdce0ccadc71234ec11bec40b4ecab64a82
LuckyGollem <123700616+LuckyGollem@users.noreply.github.com>
2023-09-02 07:38:42 +0300

    Add files via upload

---
+++ Task1.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\
+ *****   ****    *    *   \n\
+ *       *   *   *    *   \n\
+ *  **   ****    *    *   \n\
+ *   *   *   *   *    *   \n\
+ *****   *    *  ******   ");
+    return 0;
+}
